33652cdd8f4ea2217ee68e580292d44e03f019b5
bbbtttiii <ben.iburg@gmail.com>
2020-09-24 20:26:37 -0500

    cleans up migrations

--- db/migrate/20200925003447_create_users.rb
+++ db/migrate/20200925003447_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :email
       t.string :password_digest
-      t.integer :user_id
+      t.string :username
+      t.integer :u_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_010942) do
+ActiveRecord::Schema.define(version: 2020_09_25_003518) do
 
   create_table "bags", force: :cascade do |t|
     t.string "name"
@@ -34,10 +34,10 @@ ActiveRecord::Schema.define(version: 2020_09_25_010942) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.integer "user_id"
+    t.string "username"
+    t.integer "u_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
   end
 
 end
