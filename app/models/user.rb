96618542a8a63aca534da4decb9e40d1ac465d7b
bbbtttiii <ben.iburg@gmail.com>
2020-09-24 20:10:20 -0500

    adds username to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :bags
     has_many :cymbals, through: :bags
+    validates :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_003518) do
+ActiveRecord::Schema.define(version: 2020_09_25_010942) do
 
   create_table "bags", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_003518) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
