5ffce6f8d4054267741e37cb59aea7a6996e11e4
bbbtttiii <ben.iburg@gmail.com>
2020-09-24 19:55:27 -0500

    adds users_controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
-    has_secure_password
-    has_many :bags
-    has_many :cymbals, through: :bags
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect to '/'
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+
+    end
+
 end
