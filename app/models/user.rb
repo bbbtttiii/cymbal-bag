2aa847fbad2fd8985ed7b8cf26832a5c01f38e07
bbbtttiii <ben.iburg@gmail.com>
2020-09-24 20:49:21 -0500

    adds validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+
     has_many :bags
     has_many :cymbals, through: :bags
-    validates :email, presence: true
 end
