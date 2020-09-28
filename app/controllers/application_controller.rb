120c02ad3eb61c75d26cae491c1eac27ac8d4dd5
bbbtttiii <ben.iburg@gmail.com>
2020-09-28 09:52:47 -0500

    refactors appcontroller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :current_user
+    before_action :redirect_if_not_logged_in
 
     def current_user
         @current_user = User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
-        session[:user_id]
+        !!current_user
     end
 
     def redirect_if_not_logged_in
