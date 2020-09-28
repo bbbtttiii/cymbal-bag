0fe3bfa49c87aa44304977fedc78d54ae3c98323
bbbtttiii <ben.iburg@gmail.com>
2020-09-28 09:03:03 -0500

    sets up session methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :bags
   resources :cymbals
   resources :users
+
+  root 'welcome#home'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
