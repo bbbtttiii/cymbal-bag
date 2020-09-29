87a44f06be0cfa6c07798357a3d9a6d0a4420f95
bbbtttiii <ben.iburg@gmail.com>
2020-09-29 13:17:33 -0500

    adds new bag form

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -1,7 +1,11 @@
 class BagsController < ApplicationController
 
     def index
-        
+        if params[:user_id]
+            @bags = User.find_by(id: params[:user_id]).bags
+        else
+            @bags = Bags.all
+        end
     end
 
     def show
@@ -9,11 +13,11 @@ class BagsController < ApplicationController
     end
 
     def new
-
+        @bag = Bag.new
     end
 
     def create
-
+        
     end
 
     def update
@@ -21,6 +25,12 @@ class BagsController < ApplicationController
     end
 
     def destroy
+
+    end
+
+    private
+
+    def bags_params
 
     end
 
