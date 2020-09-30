bf21a5b0cf34da1c6a4489701c9dba2cba051328
bbbtttiii <ben.iburg@gmail.com>
2020-09-29 19:44:20 -0500

    adds password requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
-    validates :email, presence: true
-    validates :password, presence: true
-
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
+    
     has_many :bags
     has_many :cymbals, through: :bags
 end
